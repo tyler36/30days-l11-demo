cce7a5c39892c386c37dc3ae744c8a67731e2d36
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-03-18 10:41:35 +0900

    Style The Currently Active Route

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -29,20 +29,18 @@
                         </div>
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
-                                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <a
+                                <x-nav-link
                                     href="/"
-                                    class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-                                    aria-current="page"
-                                >Home</a>
-                                <a
+                                    :active="request()->is('/')"
+                                >Home</x-nav-link>
+                                <x-nav-link
                                     href="/contact"
-                                    class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                                >Contact</a>
-                                <a
+                                    :active="request()->is('contact')"
+                                >Contact</x-nav-link>
+                                <x-nav-link
                                     href="/about"
-                                    class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                                >About</a>
+                                    :active="request()->is('about')"
+                                >About</x-nav-link>
                             </div>
                         </div>
                     </div>
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,1 +1,9 @@
-<a {{ $attributes }}>{{ $slot }}</a>
+@props(['active' => false])
+<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+<a
+    aria-current="{{ $active ? 'page' : 'false' }}"
+    class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+    {{ $attributes }}
+>
+    {{ $slot }}
+</a>
