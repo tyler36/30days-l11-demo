acd74b69f8b90c926069b7ac49cc11ead534b74a
tyler36 <tyler36@users.noreply.github.com>
2024-03-27 10:01:27 +0900

    Two Key Relationships (#10)

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,10 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Job extends Model{
+class Job extends Model
+{
     use HasFactory;
 
     protected $table = 'job_listing';
 
     protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
