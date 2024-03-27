a00c89e0a61b35ac42028fc3c81f43d8bade91ee
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-03-27 10:00:47 +0900

    Two Key Relationships

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\Job;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Employer extends Model
 {
     use HasFactory;
+
+    public function jobs()
+    {
+        return $this->hasMany(Job::class);
+    }
 }
