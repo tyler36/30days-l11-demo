c1e8f196656b90547b518e7f24c98ad42b9eb09d
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-03-28 10:02:13 +0900

    Pivot tables and BelongsToMany

---
+++ app/Models/Tag.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Job;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+    public function jobs()
+    {
+        return $this->belongsToMany(Job::class, relatedPivotKey: 'job_listing_id');
+    }
+}
