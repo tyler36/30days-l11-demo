40e9cfae5cc2909c603814d6279c48c57c2edb37
tyler36 <tyler36@users.noreply.github.com>
2024-03-29 16:15:00 +0900

    Eager Loading and the N+1 Problem (#12)
    * add TailwindCss config
    
    * ⚙️ stop DDEV managing settings
    
    * 📦 `barryvdh/laravel-debugbar`
    
    * Eager Loading and the N+1 Problem

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,8 +1,16 @@
 <x-layout>
     <x-slot:heading>Jobs</x-slot:heading>
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li><a href="/jobs/{{ $job['id']}}" class="text-blue-600 hover:underline"><span class="font-bold">{{ $job['title'] }}</span>: Pays {{ $job['salary'] }} per year.</a></li>
+            <a
+                href="/jobs/{{ $job['id'] }}"
+                class="block px-4 py-6 border border-gray-200 rounded-lg"
+            >
+                <div class="text-sm font-bold text-blue-500">{{ $job->employer->name }}</div>
+                <div>
+                    <span class="font-bold">{{ $job['title'] }}</span>: Pays {{ $job['salary'] }} per year.
+                </div>
+            </a>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
