7a9ad3e5cb7aad0ed89610e65b41f84dd811af43
tyler36 <tyler36@users.noreply.github.com>
2024-04-09 09:36:00 +0900

    Editing, Updating and Deleting jobs (#17)

---
+++ resources/views/jobs/edit.blade.php
@@ -0,0 +1,98 @@
+<x-layout>
+    <x-slot:heading>Edit Job: {{ $job->title }}</x-slot:heading>
+
+    <form
+        method="POST"
+        action="/jobs/{{ $job->id }}"
+    >
+        @csrf
+        @method('PATCH')
+
+        <div class="space-y-12">
+            <div class="pb-12 border-b border-gray-900/10">
+                <div class="grid grid-cols-1 mt-10 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div class="sm:col-span-4">
+                        <label
+                            for="title"
+                            class="block text-sm font-medium leading-6 text-gray-900"
+                        >Title</label>
+                        <div class="mt-2">
+                            <div
+                                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                <input
+                                    type="text"
+                                    name="title"
+                                    id="title"
+                                    class="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    placeholder="Shift Leader"
+                                    required
+                                    value="{{ $job->title }}"
+                                >
+                            </div>
+
+                            @error('title')
+                                <p class="text-xs text-red-500 bold">{{ $message }}</p>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="sm:col-span-4">
+                        <label
+                            for="title"
+                            class="block text-sm font-medium leading-6 text-gray-900"
+                        >Salary</label>
+                        <div class="mt-2">
+                            <div
+                                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                <input
+                                    type="text"
+                                    name="salary"
+                                    id="salary"
+                                    class="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    placeholder="$50,000 per year"
+                                    required
+                                    value="{{ $job->salary }}"
+                                >
+                            </div>
+                            @error('salary')
+                                <p class="text-xs text-red-500 bold">{{ $message }}</p>
+                            @enderror
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+        <div class="flex items-center justify-between mt-6">
+            <div class="flex items-center">
+                <button
+                    form="delete-form"
+                    class="text-sm font-bold text-red-500"
+                >Delete</button>
+            </div>
+            <div class="flex items-center gap-x-6">
+                <a
+                    href="/jobs/{{ $job->id }}"
+                    type="button"
+                    class="text-sm font-semibold leading-6 text-gray-900"
+                >Cancel</a>
+                <div>
+                    <button
+                        type="submit"
+                        class="px-3 py-2 text-sm font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >Update</button>
+                </div>
+            </div>
+        </div>
+    </form>
+    <form
+        method="POST"
+        action="/jobs/{{ $job->id }}"
+        class="hidden"
+        id="delete-form"
+    >
+        @csrf()
+        @method('DELETE')
+    </form>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
@@ -16,10 +17,12 @@ Route::get('/jobs', function () {
     ]);
 });
 
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
@@ -28,6 +31,7 @@ Route::get('/jobs/{id}', function ($id) {
     ]);
 });
 
+// Store
 Route::post('/jobs', function () {
     // validation
     request()->validate([
@@ -41,6 +45,46 @@ Route::post('/jobs', function () {
         'employer_id' => 1,
     ]);
 
+    return redirect('/jobs');
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', [
+        'job' => $job,
+    ]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    // validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+
+    // authorize (on hold)
+
+    // update the job
+    $job = Job::findOrFail($id);
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    // redirect
+    return redirect('/jobs/' . $job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    // authorize (on hold)
+
+    Job::findOrFail($id)->delete();
+
+    // redirect
     return redirect('/jobs');
 });
 
