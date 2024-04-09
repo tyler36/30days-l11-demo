d44bf375ad552fdfeaea48d55ddb580011aea52c
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-04-09 09:35:38 +0900

    Editing, Updating and Deleting jobs

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -1,5 +1,8 @@
 <x-layout>
     <x-slot:heading>Job</x-slot:heading>
-    <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
-    <p>This job pays {{ $job['salary'] }} per year.</p>
+    <h2 class="text-lg font-bold">{{ $job->title }}</h2>
+    <p>This job pays {{ $job->salary }} per year.</p>
+    <p class="mt-6">
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
+    </p>
 </x-layout>
