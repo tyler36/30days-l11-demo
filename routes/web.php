e59f407db1d68c714eac74d8dfdc313d02595230
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-04-11 10:01:49 +0900

    Route::resource

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,6 @@ use App\Http\Controllers\JobController;
 
 Route::view('/', 'home');
 
-Route::controller(JobController::class)->group(function () {
-    Route::get('/jobs', 'index');
-    Route::get('/jobs/create', 'create');
-    Route::get('/jobs/{job}', 'show');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/{job}/edit', 'edit');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'destroy');
-});
+Route::resource('jobs', JobController::class);
 
 Route::view('/contact', 'contact');
