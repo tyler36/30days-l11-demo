dc2154c0131c29871dca9bfa804feb236979f11c
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-04-11 10:02:03 +0900

    Tidy up

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\JobController;
 
 Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
-
-Route::view('/contact', 'contact');
