da4c4d8dda9a9805cac7737db94728622ece588e
tyler36 <tyler36@users.noreply.github.com>
2024-04-23 10:23:48 +0900

    Sending mail (#22)

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\JobPosted;
+use Illuminate\Support\Facades\Mail;
 use App\Models\Job;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -38,11 +40,15 @@ class JobController extends Controller
             'salary' => ['required'],
         ]);
 
-        Job::create([
+        $job = Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1,
         ]);
+
+        Mail::to($job->employer->user)->send(
+            new JobPosted($job)
+        );
 
         return redirect('/jobs');
     }
