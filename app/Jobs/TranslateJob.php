14910cdb8493d6e438a77b4f2527820b74b25e51
tyler36 <tyler36@users.noreply.github.com>
2024-04-25 09:57:41 +0900

    Working with queues (#23)

---
+++ app/Jobs/TranslateJob.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Job;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class TranslateJob implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public Job $jobListing)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $title = $this->jobListing->title;
+
+        logger("Translating '$title' to Spanish");
+    }
+}
